c3a99adfb29056a618ec808127e49a50503a263d
20120519 <lieunguyenthi1802@gmail.com>
2023-06-09 16:56:03 +0700

    first-commt

---
+++ src/pages/playerInfor/PlayerInfor.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import DefaultLayout from "../../layout/DefaultLayout";
+import backgroundImage from "../../assets/background1.jpg";
+
+const playerInfor = () =>{
+
+}
+
+export default playerInfor
