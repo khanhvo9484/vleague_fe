7c8ce01c8b28749ace35351e2aa218ac6cbbf624
20120506 <tranduyheo123@gmail.com>
2023-06-22 14:16:09 +0700

    ko j het

--- src/api/MyAxios.js
+++ src/api/MyAxios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "https://vdhuy.azurewebsites.net/api",
-  baseURL: "https://localhost:443/api",
+  baseURL: "http://localhost:443/api",
 });
